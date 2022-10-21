777aa55159788c55a0208bf1bcb958739c7cb577
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-10-21 17:00:54 +0800

    Initial Commit

---
+++ CheatSheetIntermediate2/CheatSheetIntermediate2App.swift
@@ -0,0 +1,17 @@
+//
+//  CheatSheetIntermediate2App.swift
+//  CheatSheetIntermediate2
+//
+//  Created by Zachary on 21/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CheatSheetIntermediate2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
