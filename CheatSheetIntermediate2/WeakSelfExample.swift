61176478e3f147ad6657ec969eac432879f382a4
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-10-24 16:27:59 +0800

    Weak self

---
+++ CheatSheetIntermediate2/WeakSelfExample.swift
@@ -0,0 +1,84 @@
+//
+//  WeakSelfExample.swift
+//  CheatSheetIntermediate2
+//
+//  Created by Zachary on 24/10/22.
+//
+
+import SwiftUI
+
+struct WeakSelfExample: View {
+    
+    @AppStorage("count") var count: Int?
+    
+    init() {
+        count = 0
+    }
+    
+    var body: some View {
+        NavigationView {
+            NavigationLink("navigate", destination: WeakSelfSecondScreen())
+                .navigationTitle("Screen 1")
+        }
+        .overlay(
+            Text("\(count ?? 0)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.green.cornerRadius(10))
+            , alignment: .topTrailing
+        )
+    }
+}
+
+struct WeakSelfSecondScreen: View {
+    
+    @StateObject var vm = WeakSelfSecondScreenViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Second View")
+                .font(.largeTitle)
+                .foregroundColor(.red)
+            
+            if let data = vm.data {
+                Text(data)
+            }
+        }
+    }
+}
+
+class WeakSelfSecondScreenViewModel: ObservableObject {
+    
+    @Published var data: String? = nil
+    
+    init() {
+        print("INITIALIZE NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount + 1, forKey: "count")
+        getData()
+    }
+    
+    deinit {
+        print("DEINITIALIZE NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount - 1, forKey: "count")
+    }
+    
+    func getData() {
+        
+//        DispatchQueue.global().async {
+//            self.data = "New Data"
+//        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 500) { [weak self] in
+            self?.data = "New Data"
+        }
+        
+    }
+    
+}
+
+struct WeakSelfExample_Previews: PreviewProvider {
+    static var previews: some View {
+        WeakSelfExample()
+    }
+}
