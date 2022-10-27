116f69248bf77472a9512bc44937cfb52508c1aa
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-10-27 17:03:43 +0800

    download with Combine

--- CheatSheetIntermediate2/CheatSheetIntermediate2App.swift
+++ CheatSheetIntermediate2/CheatSheetIntermediate2App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CheatSheetIntermediate2App: App {
     var body: some Scene {
         WindowGroup {
-            DownloadWithEscapingExample()
+            DownloadWithCombineExample()
         }
     }
 }
--- CheatSheetIntermediate2/DownloadWithCombineExample.swift
+++ CheatSheetIntermediate2/DownloadWithCombineExample.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct PostModel: Identifiable, Codable {
     let userId: Int
@@ -17,22 +18,82 @@ struct PostModel: Identifiable, Codable {
 class DownloadWithCombineViewModel: ObservableObject {
     
     @Published var posts: [PostModel] = []
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        // Combine Discussion:
+        /*
+        // 1. sign up for monthly subscription for packge to be delivered
+        // 2. company would make the package behind the scene
+        // 3. receive the package at your front door
+        // 4. make sure the box isn't damaged
+        // 5. open and make sure the item is correct
+        // 6. use the item!!!
+        // 7. cancellable at anytime!!
+        
+        // 1. create the publisher
+        // 2. subscribe the publisher on background thread
+        // 3. receive on main thread
+        // 4. tryMap ( check that the data is good)
+        // 5. decode (decode data into PostModels)
+        // 6. sink (put the item into our app)
+        // 7. store (cancel subscription if needed)
+         */
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            //.subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .tryMap(handleOutput(output:))
+            .decode(type: [PostModel].self, decoder: JSONDecoder())
+            .replaceError(with: [])
+            .sink { [weak self] returnedPosts in
+                self?.posts = returnedPosts
+            }
+//            .sink { completion in
+//                switch completion {
+//                case.finished:
+//                    print("finised!")
+//                case.failure(let error) :
+//                    print("there was an error. \(error)")
+//                }
+//            } receiveValue: { [weak self] returnedPosts in
+//                self?.posts = returnedPosts
+//            }
+            .store(in: &cancellables)
+
+    }
+    
+    func handleOutput(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard
+            let response = output.response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
+        }
+        return output.data
+    }
     
 }
 
 struct DownloadWithCombineExample: View {
     
-    @State var vm = DownloadWithEscapingExample()
+    @StateObject var vm = DownloadWithCombineViewModel()
     
     var body: some View {
         List {
             ForEach(vm.posts) { post in
-                VStack {
+                VStack (alignment: .leading) {
                     Text(post.title)
                         .font(.headline)
                     Text(post.body)
                         .foregroundColor(.gray)
                 }
+                .frame(maxWidth: .infinity, alignment: .leading)
             }
         }
     }
